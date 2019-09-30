8c3b583f56f9da9ad3a401bb0ec54b01a5927bd8
Rishabh Singh <30629744+Rishabhs343@users.noreply.github.com>
2019-10-01 03:58:24 +0530

    Update seperate_tweets_from_rawDataset.py

--- seperate_tweets_from_rawDataset.py
+++ seperate_tweets_from_rawDataset.py
@@ -1,10 +1,10 @@
 import json
 from pprint import pprint
 
-with open('twitter.json') as f:
+with open('filename.json') as f:
     d = json.load(f)
     texts = []
     for data in d:
     	texts.append(data['text'])
 
-    print(texts[:100000000000000000000])
+    print(texts[0:])
